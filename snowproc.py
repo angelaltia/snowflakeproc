4d8bbdc6b92355a5e8230207f1904262c06ec8aa
Angel <angel.navacerrada@altia.es>
2024-08-05 12:54:59 +0200

    test

--- snowproc.py
+++ snowproc.py
@@ -1,4 +1,3 @@
-from snowflake.snowpark.functions import col
 def filter_by_role_git_deploy(session, table_name, role):
     df = session.table(table_name)
     return df.filter(col("role") == role)
