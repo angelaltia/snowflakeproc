bd8fa06302b9b1c5f0acdf342012a09f3a5132d6
Angel <angel.navacerrada@altia.es>
2024-08-07 12:00:27 +0200

    test 2

--- deploy.py
+++ deploy.py
@@ -1,5 +1,5 @@
 import snowflake.connector
-# Configuración de la cuenta y credenciales
+
 account = "ab35449.eu-west-1"
 user = "ANGELNAVACERRADA"
 password = "An@20240214"
@@ -7,7 +7,6 @@ warehouse = "COMPUTE_WH"
 database = "DEV_XELIO"
 schema = "DWH"
 
-# Conexión a Snowflake
 ctx = snowflake.connector.connect(
     user=user,
     password=password,
@@ -15,45 +14,6 @@ ctx = snowflake.connector.connect(
 )
 
 cs = ctx.cursor()
-
-# Leer el contenido del archivo snowproc.py
-with open('snowproc.py', 'r') as file:
-    procedure_code = file.read()
-
-# Limpiar la indentación del código Python
-procedure_code = "\n".join([line.rstrip() for line in procedure_code.split("\n")])
-
-# Insertar el código Python en el procedimiento almacenado
-create_procedure_sql = f"""
-    CREATE OR REPLACE PROCEDURE filter_by_role_procedure(
-        table_name STRING,
-        role STRING
-    )
-    RETURNS TABLE(role STRING)
-    LANGUAGE PYTHON
-    RUNTIME_VERSION = '3.8'
-    PACKAGES = ('snowflake-snowpark-python')
-    HANDLER = 'filter_by_role_git_deploy'
-    AS
-    $$
-{procedure_code}
-    $$
-"""
-
-print("SQL del Procedimiento:")
-print(create_procedure_sql)
-
-try:
-    # Cambiar al warehouse, base de datos y esquema especificados
-    cs.execute(f"USE WAREHOUSE {warehouse}")
-    cs.execute(f"USE DATABASE {database}")
-    cs.execute(f"USE SCHEMA {schema}")
-
-    # Crear o reemplazar el procedimiento almacenado en Snowflake
-    cs.execute(create_procedure_sql)
-    print("Procedimiento creado exitosamente")
-except snowflake.connector.errors.ProgrammingError as e:
-    print(f"Error al crear el procedimiento: {e}")
 
 ##################################################################
 with open('convert.py', 'r') as file:
--- snowproc.py
+++
@@ -1,5 +0,0 @@
-import snowflake.snowpark as snowpark
-from snowflake.snowpark.functions import col
-def filter_by_role_git_deploy(session, table_name, role):
-    df = session.table(table_name)
-    return df.filter(col("role") == role)
